f0a6790823e324c51dc8cf8058efe0dd10645bac
OlgierdL <146879066+OlgierdL@users.noreply.github.com>
2024-09-02 13:20:57 +0200

    Add files via upload

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 compare.py --target_path "examples/7qde/7qde#1.pdb" --model_path "examples/7qde/conformations"
+python3 casp.py -a --target_path "7qde/7qde#1.pdb" --model_path "7qde/conformations" -r --out_name "ranking1"
 
+python3 casp.py -a --target_path "7qde_ca/7qde#1.pdb" --model_path "7qde_ca/conformations" -r -c "7qde#2|A:103-113>C:103-113;7qde#10|A:103-113>A:1-11" --out_name "ranking2"
