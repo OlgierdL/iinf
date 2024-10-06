5dd9dfbff0e0fc9ec1471e855fd0dcfafa1e6c7b
Olgierd Ludwiczak <146879066+OlgierdL@users.noreply.github.com>
2024-10-06 21:07:50 +0200

    test correction

--- run_large_dataset_test.sh
+++ run_large_dataset_test.sh
@@ -1,73 +1,73 @@
 #!/bin/bash
-python3 compare.py --target_path "ITScorePR_Decoys/1C0A/target.pdb" --model_path "ITScorePR_Decoys/1C0A/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1DFU/target.pdb" --model_path "ITScorePR_Decoys/1DFU/model.pdb" -d "target|C:110-110;model|N:68-68" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1E8O/target.pdb" --model_path "ITScorePR_Decoys/1E8O/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1F7U/target.pdb" --model_path "ITScorePR_Decoys/1F7U/model.pdb" -r 
-python3 compare.py --target_path "ITScorePR_Decoys/1F7Y/target.pdb" --model_path "ITScorePR_Decoys/1F7Y/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1FFY/target.pdb" --model_path "ITScorePR_Decoys/1FFY/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1G1X/target.pdb" --model_path "ITScorePR_Decoys/1G1X/model.pdb" -d "model|I:676-676;target|E:716-716"
-python3 compare.py --target_path "ITScorePR_Decoys/1GAX/target.pdb" --model_path "ITScorePR_Decoys/1GAX/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1H3E/target.pdb" --model_path "ITScorePR_Decoys/1H3E/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1H4S/target.pdb" --model_path "ITScorePR_Decoys/1H4S/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1HQ1/target.pdb" --model_path "ITScorePR_Decoys/1HQ1/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1IL2/target.pdb" --model_path "ITScorePR_Decoys/1IL2/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1J1U/target.pdb" --model_path "ITScorePR_Decoys/1J1U/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1JBS/target.pdb" --model_path "ITScorePR_Decoys/1JBS/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1JID/target.pdb" --model_path "ITScorePR_Decoys/1JID/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1K8W/target.pdb" --model_path "ITScorePR_Decoys/1K8W/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1KOG/target.pdb" --model_path "ITScorePR_Decoys/1KOG/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1LNG/target.pdb" --model_path "ITScorePR_Decoys/1LNG/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1MMS/target.pdb" --model_path "ITScorePR_Decoys/1MMS/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1N78/target.pdb" --model_path "ITScorePR_Decoys/1N78/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1OOA/target.pdb" --model_path "ITScorePR_Decoys/1OOA/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1Q2R/target.pdb" --model_path "ITScorePR_Decoys/1Q2R/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1QTQ/target.pdb" --model_path "ITScorePR_Decoys/1QTQ/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1R3E/target.pdb" --model_path "ITScorePR_Decoys/1R3E/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1R9F/target.pdb" --model_path "ITScorePR_Decoys/1R9F/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1RC7/target.pdb" --model_path "ITScorePR_Decoys/1RC7/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1S03/target.pdb" --model_path "ITScorePR_Decoys/1S03/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1SER/target.pdb" --model_path "ITScorePR_Decoys/1SER/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1SJ3/target.pdb" --model_path "ITScorePR_Decoys/1SJ3/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1T0K/target.pdb" --model_path "ITScorePR_Decoys/1T0K/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1U0B/target.pdb" --model_path "ITScorePR_Decoys/1U0B/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/1UN6/target.pdb" --model_path "ITScorePR_Decoys/1UN6/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/1YVP/target.pdb" --model_path "ITScorePR_Decoys/1YVP/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2AKE/target.pdb" --model_path "ITScorePR_Decoys/2AKE/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2ANR/target.pdb" --model_path "ITScorePR_Decoys/2ANR/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2AZ0/target.pdb" --model_path "ITScorePR_Decoys/2AZ0/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2BH2/target.pdb" --model_path "ITScorePR_Decoys/2BH2/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2BTE/target.pdb" --model_path "ITScorePR_Decoys/2BTE/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2CSX/target.pdb" --model_path "ITScorePR_Decoys/2CSX/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2CZJ/target.pdb" --model_path "ITScorePR_Decoys/2CZJ/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2DU3/target.pdb" --model_path "ITScorePR_Decoys/2DU3/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2FK6/target.pdb" --model_path "ITScorePR_Decoys/2FK6/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2FMT/target.pdb" --model_path "ITScorePR_Decoys/2FMT/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2GJW/target.pdb" --model_path "ITScorePR_Decoys/2GJW/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2HW8/target.pdb" --model_path "ITScorePR_Decoys/2HW8/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2IPY/target.pdb" --model_path "ITScorePR_Decoys/2IPY/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2NUG/target.pdb" --model_path "ITScorePR_Decoys/2NUG/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2QUX/target.pdb" --model_path "ITScorePR_Decoys/2QUX/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2R8S/target.pdb" --model_path "ITScorePR_Decoys/2R8S/model.pdb" -d "model|A:223-223;target|B:213-213" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2RFK/target.pdb" --model_path "ITScorePR_Decoys/2RFK/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2UWM/target.pdb" --model_path "ITScorePR_Decoys/2UWM/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2V3C/target.pdb" --model_path "ITScorePR_Decoys/2V3C/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2VPL/target.pdb" --model_path "ITScorePR_Decoys/2VPL/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2XDB/target.pdb" --model_path "ITScorePR_Decoys/2XDB/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/2ZKO/target.pdb" --model_path "ITScorePR_Decoys/2ZKO/model.pdb" -d "target|A:71-71;model|B:1-1"
-python3 compare.py --target_path "ITScorePR_Decoys/2ZM5/target.pdb" --model_path "ITScorePR_Decoys/2ZM5/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2ZNI/target.pdb" --model_path "ITScorePR_Decoys/2ZNI/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2ZUE/target.pdb" --model_path "ITScorePR_Decoys/2ZUE/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/2ZZM/target.pdb" --model_path "ITScorePR_Decoys/2ZZM/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3ADD/target.pdb" --model_path "ITScorePR_Decoys/3ADD/model.pdb" -r
-python3 compare.py --target_path "ITScorePR_Decoys/3CIY/target.pdb" --model_path "ITScorePR_Decoys/3CIY/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3DD2/target.pdb" --model_path "ITScorePR_Decoys/3DD2/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3EPH/target.pdb" --model_path "ITScorePR_Decoys/3EPH/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3FOZ/target.pdb" --model_path "ITScorePR_Decoys/3FOZ/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3FTF/target.pdb" --model_path "ITScorePR_Decoys/3FTF/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3HHZ/target.pdb" --model_path "ITScorePR_Decoys/3HHZ/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3HL2/target.pdb" --model_path "ITScorePR_Decoys/3HL2/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3LRR/target.pdb" --model_path "ITScorePR_Decoys/3LRR/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3LWR/target.pdb" --model_path "ITScorePR_Decoys/3LWR/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3MOJ/target.pdb" --model_path "ITScorePR_Decoys/3MOJ/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3OL9/target.pdb" --model_path "ITScorePR_Decoys/3OL9/model.pdb"
-python3 compare.py --target_path "ITScorePR_Decoys/3OVB/target.pdb" --model_path "ITScorePR_Decoys/3OVB/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1C0A/target.pdb" --model_path "examples/ITScorePR_Decoys/1C0A/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1DFU/target.pdb" --model_path "examples/ITScorePR_Decoys/1DFU/model.pdb" -d "target|C:110-110;model|N:68-68" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1E8O/target.pdb" --model_path "examples/ITScorePR_Decoys/1E8O/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1F7U/target.pdb" --model_path "examples/ITScorePR_Decoys/1F7U/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1F7Y/target.pdb" --model_path "examples/ITScorePR_Decoys/1F7Y/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1FFY/target.pdb" --model_path "examples/ITScorePR_Decoys/1FFY/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1G1X/target.pdb" --model_path "examples/ITScorePR_Decoys/1G1X/model.pdb" -d "model|I:676-676;target|E:716-716"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1GAX/target.pdb" --model_path "examples/ITScorePR_Decoys/1GAX/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1H3E/target.pdb" --model_path "examples/ITScorePR_Decoys/1H3E/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1H4S/target.pdb" --model_path "examples/ITScorePR_Decoys/1H4S/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1HQ1/target.pdb" --model_path "examples/ITScorePR_Decoys/1HQ1/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1IL2/target.pdb" --model_path "examples/ITScorePR_Decoys/1IL2/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1J1U/target.pdb" --model_path "examples/ITScorePR_Decoys/1J1U/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1JBS/target.pdb" --model_path "examples/ITScorePR_Decoys/1JBS/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1JID/target.pdb" --model_path "examples/ITScorePR_Decoys/1JID/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1K8W/target.pdb" --model_path "examples/ITScorePR_Decoys/1K8W/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1KOG/target.pdb" --model_path "examples/ITScorePR_Decoys/1KOG/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1LNG/target.pdb" --model_path "examples/ITScorePR_Decoys/1LNG/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1MMS/target.pdb" --model_path "examples/ITScorePR_Decoys/1MMS/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1N78/target.pdb" --model_path "examples/ITScorePR_Decoys/1N78/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1OOA/target.pdb" --model_path "examples/ITScorePR_Decoys/1OOA/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1Q2R/target.pdb" --model_path "examples/ITScorePR_Decoys/1Q2R/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1QTQ/target.pdb" --model_path "examples/ITScorePR_Decoys/1QTQ/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1R3E/target.pdb" --model_path "examples/ITScorePR_Decoys/1R3E/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1R9F/target.pdb" --model_path "examples/ITScorePR_Decoys/1R9F/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1RC7/target.pdb" --model_path "examples/ITScorePR_Decoys/1RC7/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1S03/target.pdb" --model_path "examples/ITScorePR_Decoys/1S03/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1SER/target.pdb" --model_path "examples/ITScorePR_Decoys/1SER/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1SJ3/target.pdb" --model_path "examples/ITScorePR_Decoys/1SJ3/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1T0K/target.pdb" --model_path "examples/ITScorePR_Decoys/1T0K/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1U0B/target.pdb" --model_path "examples/ITScorePR_Decoys/1U0B/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1UN6/target.pdb" --model_path "examples/ITScorePR_Decoys/1UN6/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/1YVP/target.pdb" --model_path "examples/ITScorePR_Decoys/1YVP/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2AKE/target.pdb" --model_path "examples/ITScorePR_Decoys/2AKE/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2ANR/target.pdb" --model_path "examples/ITScorePR_Decoys/2ANR/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2AZ0/target.pdb" --model_path "examples/ITScorePR_Decoys/2AZ0/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2BH2/target.pdb" --model_path "examples/ITScorePR_Decoys/2BH2/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2BTE/target.pdb" --model_path "examples/ITScorePR_Decoys/2BTE/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2CSX/target.pdb" --model_path "examples/ITScorePR_Decoys/2CSX/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2CZJ/target.pdb" --model_path "examples/ITScorePR_Decoys/2CZJ/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2DU3/target.pdb" --model_path "examples/ITScorePR_Decoys/2DU3/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2FK6/target.pdb" --model_path "examples/ITScorePR_Decoys/2FK6/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2FMT/target.pdb" --model_path "examples/ITScorePR_Decoys/2FMT/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2GJW/target.pdb" --model_path "examples/ITScorePR_Decoys/2GJW/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2HW8/target.pdb" --model_path "examples/ITScorePR_Decoys/2HW8/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2IPY/target.pdb" --model_path "examples/ITScorePR_Decoys/2IPY/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2NUG/target.pdb" --model_path "examples/ITScorePR_Decoys/2NUG/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2QUX/target.pdb" --model_path "examples/ITScorePR_Decoys/2QUX/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2R8S/target.pdb" --model_path "examples/ITScorePR_Decoys/2R8S/model.pdb" -d "model|A:223-223;target|B:213-213" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2RFK/target.pdb" --model_path "examples/ITScorePR_Decoys/2RFK/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2UWM/target.pdb" --model_path "examples/ITScorePR_Decoys/2UWM/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2V3C/target.pdb" --model_path "examples/ITScorePR_Decoys/2V3C/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2VPL/target.pdb" --model_path "examples/ITScorePR_Decoys/2VPL/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2XDB/target.pdb" --model_path "examples/ITScorePR_Decoys/2XDB/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2ZKO/target.pdb" --model_path "examples/ITScorePR_Decoys/2ZKO/model.pdb" -d "target|A:71-71;model|B:1-1"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2ZM5/target.pdb" --model_path "examples/ITScorePR_Decoys/2ZM5/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2ZNI/target.pdb" --model_path "examples/ITScorePR_Decoys/2ZNI/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2ZUE/target.pdb" --model_path "examples/ITScorePR_Decoys/2ZUE/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/2ZZM/target.pdb" --model_path "examples/ITScorePR_Decoys/2ZZM/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3ADD/target.pdb" --model_path "examples/ITScorePR_Decoys/3ADD/model.pdb" -r
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3CIY/target.pdb" --model_path "examples/ITScorePR_Decoys/3CIY/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3DD2/target.pdb" --model_path "examples/ITScorePR_Decoys/3DD2/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3EPH/target.pdb" --model_path "examples/ITScorePR_Decoys/3EPH/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3FOZ/target.pdb" --model_path "examples/ITScorePR_Decoys/3FOZ/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3FTF/target.pdb" --model_path "examples/ITScorePR_Decoys/3FTF/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3HHZ/target.pdb" --model_path "examples/ITScorePR_Decoys/3HHZ/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3HL2/target.pdb" --model_path "examples/ITScorePR_Decoys/3HL2/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3LRR/target.pdb" --model_path "examples/ITScorePR_Decoys/3LRR/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3LWR/target.pdb" --model_path "examples/ITScorePR_Decoys/3LWR/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3MOJ/target.pdb" --model_path "examples/ITScorePR_Decoys/3MOJ/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3OL9/target.pdb" --model_path "examples/ITScorePR_Decoys/3OL9/model.pdb"
+python3 compare.py --target_path "examples/ITScorePR_Decoys/3OVB/target.pdb" --model_path "examples/ITScorePR_Decoys/3OVB/model.pdb" -r
